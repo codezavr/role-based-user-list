f669b79074da616e5fdc6ec14c1622ff44fb3b92
Rodion Adamenko <codezavr@gmail.com>
2023-12-11 23:29:14 +0200

    Refactor App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { AuthProvider } from './domains/auth/auth';
+import AppRouter from './router/AppRouter';
+import { NotificationsProvider } from './domains/notifications/notifications';
+
+
+const defaultTheme = createTheme();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={ defaultTheme }>
+            <CssBaseline/>
+            <AuthProvider>
+                <NotificationsProvider>
+                    <AppRouter/>
+                </NotificationsProvider>
+            </AuthProvider>
+        </ThemeProvider>
+    );
 }
 
 export default App;
